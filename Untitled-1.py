091608b27fe902e9a24e8beb769fe75c5e7316b3
Urvashi Sharma <uxs548@student.bham.ac.uk>
2026-07-26 15:24:13 +0400

    Intial comment

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+#insta check: 
+if 'term' in link_text.lower():
+    print(repr(link.text)),link.get('href')
+
+# steam check
+for link in soup.find_all("a"):
+    link_text = link.get_text(strip=True)
+    if "ssa" in link_text.lower() or "subscriber" in link_text.lower() or "terms" in link_text.lower():
+        print(repr(link_text), link.get("href"))
+
+# pinterest 
+   all_links = soup.find_all("a")
+    print(f"Total links found: {len(all_links)}") 
+    for link in all_links:
+     print(repr(link.get_text(strip=True)))
